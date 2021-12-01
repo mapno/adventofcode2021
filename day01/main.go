fc4e38dbd7df7274e05b35471a1ae34c6bc26415
Mario Rodriguez <mariorvinas@gmail.com>
2021-12-01 19:34:01 +0100

    Day 01 solution

---
+++ day01/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	// part1()
+	part2()
+}
+
+func part1() {
+	f, _ := os.Open("day01/input-0")
+
+	defer f.Close()
+
+	var prev, c int
+	s := bufio.NewScanner(f)
+	for s.Scan() {
+		d, _ := strconv.Atoi(s.Text())
+		if d > prev && prev != 0 {
+			c++
+		}
+		prev = d
+	}
+	fmt.Println(c)
+}
+
+func part2() {
+	f, _ := os.Open("day01/input-0")
+	defer f.Close()
+
+	var prev1, prev2, prev3, c int
+	s := bufio.NewScanner(f)
+	for s.Scan() {
+		d, _ := strconv.Atoi(s.Text())
+
+		if prev1 != 0 && prev1+prev2+prev3 < prev2+prev3+d {
+			c++
+		}
+
+		prev1 = prev2
+		prev2 = prev3
+		prev3 = d
+	}
+	fmt.Println(c)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mapno/adventofcode2021
+
+go 1.17
