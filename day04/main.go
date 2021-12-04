839f4c464bb3d40216a30698efdbc104e383f9b1
Mario Rodriguez <mariorvinas@gmail.com>
2021-12-04 12:45:33 +0100

    Refactor day 04

--- day04/main.go
+++ day04/main.go
@@ -9,11 +9,42 @@ import (
 )
 
 func main() {
-	// part1()
-	part2()
+	nums, boards := getInput()
+	part1(nums, boards)
+	part2(nums, boards)
 }
 
-func part1() {
+func part1(nums []int, boards [][][]int) {
+	winner, lastNum := winnerLoop(nums, boards)
+	sum := unmarkedSum(boards[winner])
+
+	fmt.Println("Part 1:", sum*lastNum)
+}
+
+func part2(nums []int, boards [][][]int) {
+	// Eliminate all boards but one
+	for _, n := range nums {
+		if len(boards) == 1 {
+			break
+		}
+		markAll(boards, n)
+
+		for { // Remove all boards that are completed in this loop
+			if complete, n := checkAll(boards); complete {
+				boards = append(boards[:n], boards[n+1:]...)
+				continue
+			}
+			break
+		}
+	}
+
+	_, lastNum := winnerLoop(nums, boards)
+	sum := unmarkedSum(boards[0])
+
+	fmt.Println("Part 2:", sum*lastNum)
+}
+
+func getInput() ([]int, [][][]int) {
 	f, _ := os.Open("day04/input-0")
 	defer f.Close()
 
@@ -44,7 +75,10 @@ func part1() {
 		}
 		boards = append(boards, board)
 	}
+	return nums, boards
+}
 
+func winnerLoop(nums []int, boards [][][]int) (int, int) {
 	var winner, lastNum int
 	for _, n := range nums {
 		lastNum = n
@@ -54,84 +88,19 @@ func part1() {
 			break
 		}
 	}
-
-	var unmarked int
-	for _, c := range boards[winner] {
-		for _, n := range c {
-			if n != 100 {
-				unmarked += n
-			}
-		}
-	}
-
-	fmt.Println(unmarked * lastNum)
+	return winner, lastNum
 }
 
-func part2() {
-	f, _ := os.Open("day04/input-0")
-	defer f.Close()
-
-	s := bufio.NewScanner(f)
-
-	var nums []int
-	s.Scan()
-	sNums := strings.Split(s.Text(), ",")
-	for _, sNum := range sNums {
-		num, _ := strconv.Atoi(sNum)
-		nums = append(nums, num)
-	}
-
-	var boards [][][]int
-	for s.Scan() {
-		board := make([][]int, 5)
-		for i := 0; i < 5; i++ {
-			board[i] = make([]int, 5)
-		}
-
-		for i := 0; i < 5; i++ {
-			s.Scan()
-			fmt.Fscanf(
-				strings.NewReader(s.Text()),
-				"%d %d %d %d %d",
-				&board[i][0], &board[i][1], &board[i][2], &board[i][3], &board[i][4])
-
-		}
-		boards = append(boards, board)
-	}
-
-	for _, n := range nums {
-		if len(boards) == 1 {
-			break
-		}
-		markAll(boards, n)
-		for {
-			if complete, n := checkAll(boards); complete {
-				boards = append(boards[:n], boards[n+1:]...)
-				continue
-			}
-			break
-		}
-	}
-
-	var lastNum int
-	for _, n := range nums {
-		lastNum = n
-		markAll(boards, n)
-		if complete, _ := checkAll(boards); complete {
-			break
-		}
-	}
-
-	var unmarked int
-	for _, c := range boards[0] {
+func unmarkedSum(board [][]int) int {
+	var sum int
+	for _, c := range board {
 		for _, n := range c {
 			if n != 100 {
-				unmarked += n
+				sum += n
 			}
 		}
 	}
-
-	fmt.Println(unmarked * lastNum)
+	return sum
 }
 
 func markAll(boards [][][]int, n int) {
