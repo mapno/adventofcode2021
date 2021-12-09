120cce48c0e541adb92d4876d2bdc49a0eb0bea7
Mario Rodriguez <mariorvinas@gmail.com>
2021-12-09 19:50:29 +0100

    Clear code

--- day09/main.go
+++ day09/main.go
@@ -15,70 +15,46 @@ func main() {
 }
 
 func part1() {
-	f, _ := os.Open("day09/input-0")
-	defer f.Close()
+	grid := getInput()
 
-	s := bufio.NewScanner(f)
-	var grid [][]int
-	var visited [][]bool
-	for s.Scan() {
-		var row []int
-		for _, c := range s.Text() {
-			row = append(row, int(c-'0'))
-		}
-		grid = append(grid, row)
-		visited = append(visited, make([]bool, len(row)))
+	visited := make([][]bool, len(grid))
+	for i := range visited {
+		visited[i] = make([]bool, len(grid[i]))
 	}
 
-	for i, row := range grid {
-		for j := range row {
-			dfs1(grid, visited, i, j)
-		}
-	}
+	dfsPart1(grid, visited, 0, 0)
 
-	fmt.Println(sum)
-
+	fmt.Println("Part 1:", sum)
 }
 
-func dfs1(grid [][]int, visited [][]bool, i, j int) {
+func dfsPart1(grid [][]int, visited [][]bool, i, j int) {
 	if i < 0 || j < 0 || i >= len(grid) || j >= len(grid[0]) || visited[i][j] {
 		return
 	}
 
+	// check if adjacent points are all greater than current point
 	if (i-1 < 0 || grid[i][j] < grid[i-1][j]) &&
 		(i+1 >= len(grid) || grid[i][j] < grid[i+1][j]) &&
 		(j-1 < 0 || grid[i][j] < grid[i][j-1]) &&
 		(j+1 >= len(grid[0]) || grid[i][j] < grid[i][j+1]) {
 		sum += grid[i][j] + 1
 	}
+
 	visited[i][j] = true
-	dfs1(grid, visited, i-1, j)
-	dfs1(grid, visited, i+1, j)
-	dfs1(grid, visited, i, j-1)
-	dfs1(grid, visited, i, j+1)
+	dfsPart1(grid, visited, i-1, j) // visit up
+	dfsPart1(grid, visited, i+1, j) // visit down
+	dfsPart1(grid, visited, i, j-1) // visit right
+	dfsPart1(grid, visited, i, j+1) // visit left
 }
 
 func part2() {
-	f, _ := os.Open("day09/input-0")
-	defer f.Close()
-
-	s := bufio.NewScanner(f)
-	var grid [][]int
-	var visited [][]bool
-	for s.Scan() {
-		var row []int
-		for _, c := range s.Text() {
-			row = append(row, int(c-'0'))
-		}
-		grid = append(grid, row)
-		visited = append(visited, make([]bool, len(row)))
-	}
+	grid := getInput()
 
 	var basins []int
 	for i, row := range grid {
 		for j := range row {
 			size = 0
-			dfs2(grid, i, j)
+			dfsPart2(grid, i, j)
 			if size > 0 {
 				basins = append(basins, size)
 			}
@@ -88,18 +64,35 @@ func part2() {
 	sort.Ints(basins)
 	l := len(basins)
 
-	fmt.Println(basins[l-1] * basins[l-2] * basins[l-3])
+	fmt.Println("Part 2:", basins[l-1]*basins[l-2]*basins[l-3])
 }
 
-func dfs2(grid [][]int, i, j int) {
+func dfsPart2(grid [][]int, i, j int) {
 	if i < 0 || j < 0 || i >= len(grid) || j >= len(grid[0]) || grid[i][j] >= 9 {
 		return
 	}
 
 	size++
 	grid[i][j] = 9
-	dfs2(grid, i-1, j)
-	dfs2(grid, i+1, j)
-	dfs2(grid, i, j-1)
-	dfs2(grid, i, j+1)
+	dfsPart2(grid, i-1, j)
+	dfsPart2(grid, i+1, j)
+	dfsPart2(grid, i, j-1)
+	dfsPart2(grid, i, j+1)
+}
+
+func getInput() [][]int {
+	f, _ := os.Open("day09/input-0")
+	defer f.Close()
+
+	s := bufio.NewScanner(f)
+	var grid [][]int
+	for s.Scan() {
+		var row []int
+		for _, c := range s.Text() {
+			row = append(row, int(c-'0'))
+		}
+		grid = append(grid, row)
+	}
+
+	return grid
 }
